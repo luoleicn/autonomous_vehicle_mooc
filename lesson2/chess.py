757a003fd882027f668d3d5d54d6c17fa059d84f
wb-ll340921 <wb-ll340921@cainiao.com>
2018-08-13 16:43:21 +0800

    perspective_transform

--- lesson2/chess.py
+++ lesson2/chess.py
@@ -8,6 +8,7 @@ nx = 8
 ny = 6#TODO: enter the number of inside corners in y
 
 # Make a list of calibration images
+#fname = 'test_image2.png'
 fname = 'calibration_test.png'
 img = cv2.imread(fname)
 
@@ -16,9 +17,9 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Find the chessboard corners
 ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-#print corners
-print gray.shape
-print gray.shape[::-1]
+print corners
+#print gray.shape
+#print gray.shape[::-1]
 
 # If found, draw corners
 if ret == True:
